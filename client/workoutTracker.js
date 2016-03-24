a43b2f3db94ba7d9af7edfc4ef5d049999e1958c
Zach Whitten <zach.whitten@rivetlogic.com>
2016-03-24 16:04:05 -0400

    most of the rest completed

--- client/workoutTracker.js
+++ client/workoutTracker.js
@@ -1,6 +1,8 @@
 //Meteor.subscribe("workouts");
 Session.setDefault('limit', 10);
 Session.setDefault('sorting',-1);
+
+DistanceByMonth = new Mongo.Collection('distanceByMonth');
 //Subscriptions
 Tracker.autorun(function(computation){
   Meteor.subscribe('workouts', {
@@ -29,4 +31,45 @@ Template.workoutList.events({
     Session.set('sorting', newSort);
   }
 
+});
+
+Template.addWorkout.events({
+  'submit form': function(evt, tpl){
+    evt.preventDefault();
+
+    var distance = parseInt(tpl.$('input[name="distance"]').val());
+    Meteor.call('CreateWorkout', {
+      distance: distance
+    }, function(error, result){
+      if(error) return alert('Error: '+error.error);
+    });
+  }
+});
+
+monthLabels = {
+  1: 'January',
+  2: 'February',
+  3: 'March',
+  4: 'April',
+  5: 'Mai',
+  6: 'June',
+  7: 'July',
+  8: 'August',
+  9: 'September',
+  10: 'October',
+  11: 'November',
+  12: 'December'
+};
+
+Template.distanceByMonth.helpers({
+  month: function () {
+    return DistanceByMonth.find({}, {
+      sort: {
+        _id: 1
+      }
+    });
+  },
+  translateMonth: function () {
+    return monthLabels[this._id];
+  }
 });
--- server/publications.js
+++ server/publications.js
@@ -16,3 +16,48 @@ Meteor.publish('workouts', function (options) {
 
   return WorkoutsCollection.find(qry, qryOptions);
 });
+
+Meteor.publish("distanceByMonth", function(argument){
+  var subsciption = this;
+  var initiated = false;
+  var distances = {};
+  var userId = this.userId;
+
+  var db = MongoInternals.defaultRemoteCollectionDriver().mongo.db;
+  var pipeline = [{$match: {userId: userId}},
+    {
+      $group: {
+        _id: { $month: '$workoutAt'},
+        distance: {$sum: '$distance'}
+      }
+    }
+  ];
+
+  db.collection('workouts').aggregate(
+    pipeline,
+    Meteor.bindEnvironment(
+      function(err, result){
+        console.log('result', result);
+        _.each(result, function(r){
+          subsciption.added('distanceByMonth', r._id, {distance: r.distance});
+        })
+      }
+    )
+  );
+
+  var workoutHandle = WorkoutsCollection
+    .find({userId: userId})
+    .observeChanges({
+      added: function(id, fields){
+        if(!initiated) return;
+        idByMonth = new Date(fields.workoutAt).getMonth()+1;
+        distances[idByMonth] += fields.distance;
+        subsciption.changed('distanceByMonth', idByMonth, { distance: distances[idByMonth]});
+      }
+    });
+    initiated = true;
+    subsciption.onStop(function(){
+      workoutHandle.stop();
+    });
+    subsciption.ready();
+});
